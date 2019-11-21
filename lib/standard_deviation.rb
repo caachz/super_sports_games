a96c673f2552d1ecbf6f8630d4e4f4a31ad86f4f
Cassie Achzenick <caachzenick@gmail.com>
2019-11-21 13:29:54 -0700

    Calculate total ages and sum of ages

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,16 @@
+
 ages = [24, 30, 18, 20, 41]
+sum_of_ages = 0
+total_ages = ages.count
 
 # Your code here for calculating the standard deviation
+    ages.each do |age|
+      sum_of_ages += age
+    end
+
+mean = sum_of_ages/total_ages
+
+p total_ages
+p amount_of_ages
 
 # When you find the standard deviation, print it out
