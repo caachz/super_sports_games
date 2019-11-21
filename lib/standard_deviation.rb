235c8c1e5205f0410a3860cd8b5dde500b50c9a9
Cassie Achzenick <caachzenick@gmail.com>
2019-11-21 13:50:56 -0700

    Add up to step 7 of standard deviation

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -2,6 +2,9 @@
 ages = [24, 30, 18, 20, 41]
 sum_of_ages = 0
 total_ages = ages.count
+diff_between_actual_and_mean = []
+diff_squared = []
+sum_of_diff_squared = 0
 
 # Your code here for calculating the standard deviation
     ages.each do |age|
@@ -10,7 +13,26 @@ total_ages = ages.count
 
 mean = sum_of_ages/total_ages
 
+    ages.each do |age|
+      diff_between_actual_and_mean << age - mean
+    end
+
+    diff_between_actual_and_mean.each do |age|
+      diff_squared << age * age
+    end
+
+    diff_squared.each do |age|
+      sum_of_diff_squared += age
+    end
+
+sum_of_diff_squared_devided_by_count = sum_of_diff_squared/total_ages
+
+p sum_of_ages
 p total_ages
-p amount_of_ages
+p mean
+p diff_between_actual_and_mean
+p diff_squared
+p sum_of_diff_squared
+p sum_of_diff_squared_devided_by_count
 
 # When you find the standard deviation, print it out
